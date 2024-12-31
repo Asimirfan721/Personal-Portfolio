b028a08aecad3725d735f0120e27a5ae15786622
Asim Irfan <rajputasim721@gmail.com>
2024-12-31 14:44:23 +0500

    small changes

--- database/migrations/2024_08_06_120732_create_personal_statements_table.php
+++ database/migrations/2024_08_06_120732_create_personal_statements_table.php
@@ -6,22 +6,20 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
-class AddUserIdToPersonalStatementsTable extends Migration
+class CreatePersonalStatementsTable extends Migration
 {
     public function up()
     {
-        Schema::table('personal_statements', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id')->after('id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        Schema::create('personal_statements', function (Blueprint $table) {
+            $table->id();
+            $table->string('statement');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::table('personal_statements', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
-            $table->dropColumn('user_id');
-        });
+        Schema::dropIfExists('personal_statements');
     }
 }
