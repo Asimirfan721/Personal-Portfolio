4dd29d2e5dcd42f02e15bbbd7b67d5a862c7778b
Asimirfan721 <rajputasim721@gmail.com>
2025-02-08 16:05:26 +0500

    controller

--- app/Http/Controllers/PersonalStatementController.php
+++ app/Http/Controllers/PersonalStatementController.php
@@ -12,7 +12,7 @@ class PersonalStatementController extends Controller
 {
     public function __construct()
     {
-        // Ensure only auts can access these methods
+        // Ensure only aucan access these methods
       //  $this->middleware('auth');
     }
 
