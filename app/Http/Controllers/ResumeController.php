3d54614c24d905a6bbed9d56fa5fd5064db0ad71
Asimirfan721 <rajputasim721@gmail.com>
2025-02-16 23:38:44 +0500

    Resume

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -22,10 +22,9 @@ class ResumeController extends Controller
             'description' => 'required|string|max:255',
         ]);
 
-        // Store file in the 'public/resume' 
+         
         $path = $request->file('file')->store('resume', 'public');
 
-        // Save description hi thi s is 
         $descriptions = json_decode(Storage::get('public/resume/descriptions.json') ?? '[]', true);
         $descriptions[$path] = $request->description;
         Storage::put('public/resume/descriptions.json', json_encode($descriptions));
