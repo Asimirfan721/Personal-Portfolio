663a8d6d56831f7de893e3d9f0765fa4469c82d0
Asimirfan721 <rajputasim721@gmail.com>
2025-02-16 23:41:45 +0500

    migrations

--- database/migrations/2024_08_07_045335_create_statements_of_purpose_table.php
+++ database/migrations/2024_08_07_045335_create_statements_of_purpose_table.php
@@ -23,11 +23,7 @@ class CreateStatementsOfPurposeTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         Schema::dropIfExists('statements_of_purpose');
