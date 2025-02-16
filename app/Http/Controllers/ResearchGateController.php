733c0e1ac5f77d74d776cf976f83d4976669e42e
Asimirfan721 <rajputasim721@gmail.com>
2025-02-16 23:37:35 +0500

    researchgate

--- app/Http/Controllers/ResearchGateController.php
+++ app/Http/Controllers/ResearchGateController.php
@@ -10,7 +10,7 @@ class ResearchGateController extends Controller
 {
     public function index()
     {
-        // Replace the U RL below withle URL
+        
         $researchGateUrl = 'https://www.researchgate.net/profile/Asim-Irfan';
  
         return redirect()->away($researchGateUrl);
