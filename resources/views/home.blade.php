5c67fd883c23dc2f76555a5246a8eaf008789c08
Asim Irfan <rajputasim721@gmail.com>
2025-02-27 17:43:31 +0500

    perrsonal statement removed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -131,8 +131,7 @@
             <a class="nav-link" href="{{ route('github') }}">GitHub</a>
             <a class="nav-link" href="{{ route('researchgate') }}">ResearchGate</a>
             <a class="nav-link" href="{{ route('coursera.showButtons') }}">Certifications</a>
-            <a class="nav-link" href="{{ route('personal-statement.index') }}">Personal Statement</a>
-            <a class="nav-link" href="{{ route('statement-of-purpose') }}">Statement of Purpose</a>
+             <a class="nav-link" href="{{ route('statement-of-purpose') }}">Statement of Purpose</a>
             <a class="nav-link" href="{{ route('Calculation') }}">Calculation</a>
             <a class="nav-link" href="{{ route('resume') }}">Resume</a>
 
