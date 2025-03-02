2d7d348b1b9d41262602a1f0ccc062674747305e
Asimirfan721 <rajputasim721@gmail.com>
2025-03-02 23:59:56 +0500

    m

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -35,10 +35,10 @@ return new class extends Migration
         });
 
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
+            $table->id();  
             $table->string('uuid')->unique();
             $table->text('connection');
-            $table->text('queue');
+            $table->text('queue');   
             $table->longText('payload');   
             $table->longText('exception');
             $table->timestamp('failed_at')->useCurrent();
