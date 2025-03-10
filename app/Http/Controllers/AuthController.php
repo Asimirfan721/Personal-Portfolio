e2c143f0fe4a729a1674602a716cc534344f7890
Asim Irfan <rajputasim721@gmail.com>
2025-03-10 11:39:22 +0500

    authcontroller's comment completed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,18 +3,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use App\Models\User; //model with name user is imported
+use Illuminate\Http\Request; //request class is imported
+use Illuminate\Support\Facades\Auth;//auth class is imported
+use Illuminate\Support\Facades\Hash; //hash class is imported
+use Illuminate\Support\Facades\Validator;//validator class is imported
 
-class AuthController extends Controller 
+class AuthController extends Controller //class AuthController is created which extends Controller class
 {
      
-    public function showRegisterForm()
+    public function showRegisterForm() //showRegisterForm function is created
     {
-        return view('auth.register');
+        return view('auth.register'); // This is the view that will be returned when the showRegisterForm function is called
     }
 
     /**
@@ -23,21 +23,21 @@ class AuthController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function register(Request $request)
+    public function register(Request $request) // register function is created, where request variable is used as a parameter 
     {
-        $this->validateRegistration($request);
+        $this->validateRegistration($request); // validateRegistration function is called with request varibale as a parameter
 
         // Create the user
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+        $user = User::create([ //user is created with the following fields
+            'name' => $request->name, //name field is created with the value of name field from the request
+            'email' => $request->email,//email field is created with the value of email field from the request
+            'password' => Hash::make($request->password), //password field is created with the value of password field from the request
         ]);
 
         // Automatically log the user in after registration (optional)
-        Auth::login($user);
+        Auth::login($user); //user is logged in
 
-        return redirect()->route('login')->with('success', 'Registration successful! Welcome aboard.');
+        return redirect()->route('login')->with('success', 'Registration successful! Welcome aboard.'); //redirect to route login page with success message
     }
 
     /**
@@ -45,9 +45,9 @@ class AuthController extends Controller
      *
      * @return \Illuminate\View\View
      */
-    public function showLoginForm()
+    public function showLoginForm() //showLoginForm function is created
     {
-        return view('auth.login');
+        return view('auth.login');   // This is the view that will be returned when the showLoginForm function is called
     }
 
     /**
@@ -56,20 +56,20 @@ class AuthController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function login(Request $request)
+    public function login(Request $request) //login function is created, where request variable is used as a parameter  
     {
-        $this->validateLogin($request);
+        $this->validateLogin($request); // validateLogin function is called with request varibale as a parameter
 
-        $credentials = $request->only('email', 'password');
-
-        if (Auth::attempt($credentials)) {
+        $credentials = $request->only('email', 'password');   //credentials is created with email and password fields from the request
+ 
+        if (Auth::attempt($credentials)) {                   //if the credentials are correct
             // Regenerate session ID to prevent session fixation
             $request->session()->regenerate();
 
-            return redirect()->route('home')->with('success', 'You are now logged in!');
+            return redirect()->route('home')->with('success', 'You are now logged in!'); // redirect to home page with success message
         }
 
-        return redirect()->back()->withErrors(['email' => 'Invalid credentials provided.'])->withInput();
+        return redirect()->back()->withErrors(['email' => 'Invalid credentials provided.'])->withInput(); //if the credentials are incorrect return back with error message
     }
 
     /**
@@ -78,14 +78,14 @@ class AuthController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function logout(Request $request)
+    public function logout(Request $request) // logout function is created, where request variable is used as a parameter
     {
-        Auth::logout();
+        Auth::logout(); //user is logged out
 
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
+        $request->session()->invalidate(); //session is invalidated
+        $request->session()->regenerateToken(); //token is regenerated
 
-        return redirect('/login')->with('success', 'You have successfully logged out.');
+        return redirect('/login')->with('success', 'You have successfully logged out.'); // redirect to login page with success message
     }
 
     /**
@@ -94,9 +94,9 @@ class AuthController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return void
      */
-    protected function validateRegistration(Request $request)
+    protected function validateRegistration(Request $request) //validateRegistration function is created, where request variable is used as a parameter
     {
-        $request->validate([
+        $request->validate([ //request is validated with the following fields
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
@@ -109,9 +109,9 @@ class AuthController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return void
      */
-    protected function validateLogin(Request $request)
+    protected function validateLogin(Request $request) //validateLogin function is created, where request variable is used as a parameter
     {
-        $request->validate([
+        $request->validate([ //request is validated with the following fields
             'email' => ['required', 'string', 'email'],
             'password' => ['required', 'string'],
         ]);
