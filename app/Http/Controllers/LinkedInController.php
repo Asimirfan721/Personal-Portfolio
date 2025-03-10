62615074bb75dd4d1c01c9c47cf5d12f29a4093c
Asim Irfan <rajputasim721@gmail.com>
2025-03-10 12:29:30 +0500

    linkedin Done

--- app/Http/Controllers/LinkedInController.php
+++ app/Http/Controllers/LinkedInController.php
@@ -1,16 +1,17 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers; // namespace is defined 
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request;    // Request class is imported
+use App\Http\Controllers\Controller; // Controller class is imported
 
-class LinkedInController extends Controller
+class LinkedInController extends Controller // LinkedInController class is defined and extends Controller class
 {
-    public function index() 
+    public function index() //index method is defined
     { 
         // Replace the URL   below act linkedIn profile URL url of linkedin
         $linkedinUrl = 'https://www.linkedin.com/in/asim-irfan-aa49a31a1/';
 
-        return redirect()->away($linkedinUrl); 
+        return redirect()->away($linkedinUrl);  // Redirects to the LinkedIn URL
     }
 }
