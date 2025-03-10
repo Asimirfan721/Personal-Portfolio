4ba118ec3778aef4a517bb8d4ec7997afa7f7506
Asim Irfan <rajputasim721@gmail.com>
2025-03-10 12:23:34 +0500

    github Controller

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -1,17 +1,18 @@
 <?php
 
 // app/Http/Controllers/GitHubController.
-namespace App\Http\Controllers;
+namespace App\Http\Controllers; // namespace is defined 
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; // Request class is imported
+use App\Http\Controllers\Controller; // Controller class is imported
 
-class GitHubController extends Controller
+class GitHubController extends Controller // GitHubController class is defined and extends Controller class 
 {
-    public function index()
+    public function index() // index method is defined
     {  
         // Fetch GitHu  b   URLfrom the environment file direct connects to the GitHub URL
         $githubUrl = config('services.github.url', 'https://github.com/Asimirfan721');
 
-        return redirect()->away($githubUrl);
+        return redirect()->away($githubUrl); // Redirects to the GitHub URL
     }
 }
