658b57e75433203edb7ef4bc93706d90aec284a2
Asimirfan721 <rajputasim721@gmail.com>
2025-03-15 23:32:52 +0500

    Homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,9 +8,9 @@ use Illuminate\Http\Request; // Request class is imported
 use App\Http\Controllers\Controller; // Controller class is imported
 
 class HomeController extends Controller // HomeController class is defined and extends Controller class
-{
+{   
     public function index() // index method is defined
     {// Return the home view
         return view('home'); // view is called with name home
     }  
-} 
+}  
