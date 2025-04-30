36005b549ec9fc5a1386d08e286f0b300a9a4117
Asimirfan721 <rajputasim721@gmail.com>
2025-04-30 23:46:25 +0500

    github attached

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -6,11 +6,11 @@ namespace App\Http\Controllers; // namespace is defined
 use Illuminate\Http\Request; // Request class is imported
 use App\Http\Controllers\Controller; // Controller class is imported
 
-class GitHubController extends Controller // GitHubController class is defined and extends Controller class 
+class GitHubController extends Controller // GitHubController class is defined and extends Controller class  git hub attached
 {
     public function index() // index method is defined
     {  
-        // Fetch GitHu  b   URLfrom the environment file direct connects to the GitHub URL
+        // Fetch GitHu  b   URLfrom the environment file direct connects to the GitHub URL    //
         $githubUrl = config('services.github.url', 'https://github.com/Asimirfan721');
 
         return redirect()->away($githubUrl); // Redirects to the GitHub URL
