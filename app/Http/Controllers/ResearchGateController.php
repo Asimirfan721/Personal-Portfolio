2bf827f674405377932f6b702081f4ba97a90869
Asimirfan721 <rajputasim721@gmail.com>
2025-04-30 23:48:18 +0500

    coursera attached

--- app/Http/Controllers/ResearchGateController.php
+++ app/Http/Controllers/ResearchGateController.php
@@ -12,7 +12,7 @@ class ResearchGateController extends Controller
     {
           
         $researchGateUrl = 'https://www.researchgate.net/profile/Asim-Irfan';
- // let's add something in this profile 
+ // let's add something in this profile here
         return redirect()->away($researchGateUrl);   
     }
 }
